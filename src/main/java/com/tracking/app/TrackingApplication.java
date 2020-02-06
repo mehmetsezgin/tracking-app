847aea8f6d25e154acfb967c8fb7975a28b1cbad
Mehmet Sezgin <m.sezgin@emakina.com.tr>
2020-02-06 15:07:18 +0300

    Class renaming

--- src/main/java/com/tracking/app/TrackingApplication.java
+++ src/main/java/com/tracking/app/TrackingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrackingAppApplication {
+public class TrackingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrackingAppApplication.class, args);
+		SpringApplication.run(TrackingApplication.class, args);
 	}
 
 }
